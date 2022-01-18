f20ac4561b48405a949b93b4e906133cfa1f90f1
Lia Moua <moua0061@yahoo.com>
2022-01-17 21:38:23 -0500

    completed migration up and down

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+    tbl.text('VIN', 256).unique().notNullable()//required, unique
+    tbl.text('Make', 256).notNullable()//required
+    tbl.text('Model', 256).notNullable()//required
+    tbl.float('Mileage').notNullable()//required
+    tbl.text('Title') //optional
+    tbl.text('Transmission') //optional
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
